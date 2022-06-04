e16c21210996d3e37ff771370eb9db4f1e27762b
Mustafa <mustizena@gmail.com>
2022-06-04 16:25:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var socket = require('socket.io')
+
+//App setup
+
+var app = express();
+var server = app.listen(4000, function(){
+ console.log('listening to requests on port 4000');
+});
+
+//Static Files
+
+app.use(express.static('public'));
+
+//Socket setup
+
+var io = socket(server);
+
+io.on('connection',function(socket){
+ console.log('made socket connection',socket.id);
+
+//handle chat event
+ socket.on('chat',function(data){
+  io.sockets.emit('chat',data)
+ });
+ socket.on('typing', function (data) {
+   socket.broadcast.emit('typing',data)
+});
+});
+
+ 
+
+
+
+
+
+
+
